8e5eb1c9c55f34abab184a6cc5222f89cfe7054c
Yutong Wang <49324455+yutongw0427@users.noreply.github.com>
2019-05-01 12:42:28 -0500

    Update Flight Routes Analysis.sh

--- CodeFiles/Flight Routes Analysis.sh
+++ CodeFiles/Flight Routes Analysis.sh
@@ -1,5 +1,2 @@
-#Creating table with the number airline routes:
-hive -e "SELECT Year,Origin, Dest FROM flights" > route.csv
-
 #Creating table with the number of flights for destinations(done in hive):
 hive -e "SELECT Year, Dest, count (Dest) FROM flights GROUP BY Year, Dest" > dest.csv
