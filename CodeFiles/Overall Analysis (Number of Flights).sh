a328f6dea5d1889d0e64ec381371625555b31719
Yutong Wang <49324455+yutongw0427@users.noreply.github.com>
2019-05-01 09:31:18 -0500

    Update Overall Analysis (Number of Flights).sh

--- CodeFiles/Overall Analysis (Number of Flights).sh
+++ CodeFiles/Overall Analysis (Number of Flights).sh
@@ -1,16 +1,3 @@
-# Downloading the necessary data (done in terminal)
-wget https://raw.githubusercontent.com/coatless/stat490uiuc/master/airlines/airlines_data.sh
-chmod u+x airlines_data.sh
-./airlines_data.sh 2002 2002
-mv airlines.csv groupproject.csv
-./airlines_data.sh 1998 1998
-tail -n+2 airlines.csv >> groupproject.csv
-
-# Deleting columns with all data missing
-cut -d ',' -f 23,25,26,27,28,29 --complement groupproject.csv > groupprojectcl.csv
-
-
-
 #Creating table with the daily number of flights(Done in hive):
 hive -e "SELECT Year, Month, DayofMonth, count (DayofMonth) FROM flights GROUP BY Year, Month, DayofMonth" > count_day.csv
 
